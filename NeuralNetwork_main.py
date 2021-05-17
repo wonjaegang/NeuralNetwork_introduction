575eaa284bf0de6472c5ecc73e971ff1bd090d7c
Wonjaegang <robinsam@naver.com>
2021-05-17 18:48:42 +0900

    Initial commit

---
+++ NeuralNetwork_main.py
@@ -0,0 +1,12 @@
+import random
+
+
+class Node:
+    def __init__(self, layer, order, bias):
+        self.layer = layer
+        self.order = order
+        self.bias = bias
+
+
+if __name__ == "__main__":
+    pass
