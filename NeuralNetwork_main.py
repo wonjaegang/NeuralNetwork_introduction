9fa0ea980ed0df97d58eb7a9fc6a20d2728d967e
Wonjaegang <robinsam@naver.com>
2021-05-17 22:39:47 +0900

    Change class declaration: Layer
    - 각 노드들을 하나의 인스턴스들로 선언해주는 것 보다 각 레이어들을 하나의 인스턴스로 정의했을 때의 이점이 더 크다고 생각하여, 원래의 노드 클래스의 역할을 레이어 클래스에게로 이전하였다.

--- NeuralNetwork_main.py
+++ NeuralNetwork_main.py
@@ -1,12 +1,30 @@
 import random
 
 
-class Node:
-    def __init__(self, layer, order, bias):
-        self.layer = layer
-        self.order = order
-        self.bias = bias
+def activationFunc(x):
+    return x
+
+
+class Layer:
+    def __init__(self, layerIndex, size, lastLayerSize):
+        self.layerIndex = layerIndex
+        self.size = size
+        self.lastLayerSize = lastLayerSize
+        self.nodeBias = [random.random() for _ in range(size)]
+        self.nodeValue = self.calculateNodeValue()
+
+    def calculateNodeValue(self):
+        Z = 0
+        for nodeIndex in range(self.size):
+            pass
+        return activationFunc(Z)
 
 
 if __name__ == "__main__":
-    pass
+    inputValue = []
+    layer1 = Layer(0, 8, len(inputValue))
+    layer2 = Layer(1, 8, layer1.size)
+    outputLayer = Layer(2, 1, layer2.size)
+    print(layer1.nodeBias)
+    print(layer2.nodeBias)
+    print(outputLayer.nodeBias)
