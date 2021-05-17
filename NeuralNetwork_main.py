ccb4b32a4e345991a4d4a8fae32c38772a03872c
Wonjaegang <robinsam@naver.com>
2021-05-17 23:18:48 +0900

    Add layer class method: calculateNodeValue
    - 레이어 클래스의 calculateNodeValue 추가하였다. 레이어의 각 노드들은 이전 레이어의 각 노드값 * 가중치를 모두 더한값에, 편향(bias)를 더한 값이 된다(여기서 활성화함수는 항등함수이므로 무시).
    - 이전버전에서 입력레이어를 리스트로 나타냈었다면, 직관성을 위해 입력레이어 또한 레이어 클래스의 인스턴수로 선언하였다. 이를 위해 레이어 클래스의 초기화함수에 If 문을 추가하였다.

--- NeuralNetwork_main.py
+++ NeuralNetwork_main.py
@@ -2,29 +2,51 @@ import random
 
 
 def activationFunc(x):
-    return x
+    y = x
+    return y
 
 
 class Layer:
-    def __init__(self, layerIndex, size, lastLayerSize):
+    def __init__(self, layerIndex, size, lastLayer):
         self.layerIndex = layerIndex
         self.size = size
-        self.lastLayerSize = lastLayerSize
-        self.nodeBias = [random.random() for _ in range(size)]
-        self.nodeValue = self.calculateNodeValue()
+        # If it is an input layer
+        if not lastLayer:
+            self.nodeValue = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        # If it not
+        else:
+            self.lastLayer = lastLayer
+            self.nodeBias = [random.random() for _ in range(size)]
+            # 이전 레이어와 연결되는 신경망들의 가중치
+            self.neuronWeight = [[random.random() for _ in range(lastLayer.size)] for __ in range(size)]
+            self.nodeValue = self.calculateNodeValue()
 
     def calculateNodeValue(self):
-        Z = 0
+        Z = []
         for nodeIndex in range(self.size):
-            pass
+            value = 0
+            for lastNodeIndex in range(self.lastLayer.size):
+                value = value + self.lastLayer.nodeValue[lastNodeIndex] * self.neuronWeight[nodeIndex][lastNodeIndex]
+            Z.append(value + self.nodeBias[nodeIndex])
         return activationFunc(Z)
 
 
 if __name__ == "__main__":
-    inputValue = []
-    layer1 = Layer(0, 8, len(inputValue))
-    layer2 = Layer(1, 8, layer1.size)
-    outputLayer = Layer(2, 1, layer2.size)
-    print(layer1.nodeBias)
-    print(layer2.nodeBias)
-    print(outputLayer.nodeBias)
+    inputLayer = Layer(0, 10, False)
+    layer1 = Layer(1, 8, inputLayer)
+    layer2 = Layer(2, 8, layer1)
+    outputLayer = Layer(3, 1, layer2)
+
+    print("inputLayer node value:", inputLayer.nodeValue)
+
+    print("\nlayer1 node bias:", layer1.nodeBias)
+    print("layer1 neuron weight:", layer1.neuronWeight)
+    print("layer1 node value:", layer1.nodeValue)
+
+    print("\nlayer2 node bias:", layer2.nodeBias)
+    print("layer2 neuron weight:", layer2.neuronWeight)
+    print("layer2 node value:", layer2.nodeValue)
+
+    print("\noutputLayer node bias:", outputLayer.nodeBias)
+    print("outputLayer neuron weight:", outputLayer.neuronWeight)
+    print("outputLayer node value:", outputLayer.nodeValue)
