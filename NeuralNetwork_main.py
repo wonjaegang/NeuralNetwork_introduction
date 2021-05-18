b705134f4c98c483d259b7dbcfd13e2280b44c8f
Wonjaegang <robinsam@naver.com>
2021-05-18 12:16:46 +0900

    Add functions: randomList & expectedOutput
    - 두 함수(randomList & expectedOutput) 모두 입/출력에 관련된 함수로, randomList 는 편향/가중치/입력값에 사용되며 expectedOutput 은 입력값에 따른 원하는 결괏값을 반환한다.

--- NeuralNetwork_main.py
+++ NeuralNetwork_main.py
@@ -12,23 +12,35 @@ class Layer:
         self.size = size
         # If it is an input layer
         if not lastLayer:
-            self.nodeValue = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+            self.nodeValue = randomList(size)
         # If it not
         else:
             self.lastLayer = lastLayer
-            self.nodeBias = [random.random() for _ in range(size)]
+            self.nodeBias = randomList(size)
             # 이전 레이어와 연결되는 신경망들의 가중치
-            self.neuronWeight = [[random.random() for _ in range(lastLayer.size)] for __ in range(size)]
+            self.neuronWeight = [randomList(lastLayer.size) for _ in range(size)]
             self.nodeValue = self.calculateNodeValue()
 
     def calculateNodeValue(self):
-        Z = []
+        valueList = []
         for nodeIndex in range(self.size):
             value = 0
             for lastNodeIndex in range(self.lastLayer.size):
                 value = value + self.lastLayer.nodeValue[lastNodeIndex] * self.neuronWeight[nodeIndex][lastNodeIndex]
-            Z.append(value + self.nodeBias[nodeIndex])
-        return activationFunc(Z)
+            value = activationFunc(value + self.nodeBias[nodeIndex])
+            valueList.append(value)
+        return valueList
+
+
+def randomList(size):
+    return [random.random() for _ in range(size)]
+
+
+def expectedOutput():
+    if sum(inputLayer.nodeValue) > inputLayer.size / 2:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == "__main__":
@@ -37,7 +49,7 @@ if __name__ == "__main__":
     layer2 = Layer(2, 8, layer1)
     outputLayer = Layer(3, 1, layer2)
 
-    print("inputLayer node value:", inputLayer.nodeValue)
+    print("Input layer node value:", inputLayer.nodeValue)
 
     print("\nlayer1 node bias:", layer1.nodeBias)
     print("layer1 neuron weight:", layer1.neuronWeight)
@@ -47,6 +59,6 @@ if __name__ == "__main__":
     print("layer2 neuron weight:", layer2.neuronWeight)
     print("layer2 node value:", layer2.nodeValue)
 
-    print("\noutputLayer node bias:", outputLayer.nodeBias)
-    print("outputLayer neuron weight:", outputLayer.neuronWeight)
-    print("outputLayer node value:", outputLayer.nodeValue)
+    print("\nOutput layer node bias:", outputLayer.nodeBias)
+    print("Output layer neuron weight:", outputLayer.neuronWeight)
+    print("Output layer node value:", outputLayer.nodeValue)
