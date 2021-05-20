5b576c5babce8aa27dff244a661fd752abdc6a25
Wonjaegang <robinsam@naver.com>
2021-05-20 21:42:14 +0900

    Revise codes for intuitiveness
    - 레이어클래스의 변수들을 긴 이름이 아닌 행렬기호로 설정하고, 주석으로 수식을 추가하여 코드가 더 직관적이도록 하였다.
    - 더 일반적인 상황을 위해, 비용함수의 수식과 기대출력값 함수의 반환값을 수정하였다. 이를 통해 추후에 이 비용함수를 출력이 두 개 이상인 출력레이어에도 사용할 수 있다.

--- NeuralNetwork_main.py
+++ NeuralNetwork_main.py
@@ -8,36 +8,46 @@ class Layer:
         self.size = size
         # If it is an input layer
         if not lastLayer:
-            self.nodeValue = randomList(size, 0, 1)
+            # 입력값들로 이루어진 열벡터 A
+            self.A = randomList(size, 0, 1)
         # If it not
         else:
             self.lastLayer = lastLayer
-            self.nodeBias = randomList(size, -2, 2)
 
-            # 이전 레이어와 연결되는 신경망들의 가중치
-            self.neuronWeight = [randomList(lastLayer.size, -2, 2) for _ in range(size)]
+            # 레이어 노드들의 편향값들로 이루어진 열벡터 B
+            self.B = randomList(size, -1, 1)
 
-            # 레이어의 각 노드들의 z(활성화함수 입력값)와 실제 노드의 값(활성화함수 출력값)을 구한다
+            # 이전 레이어와 연결되는 신경망들의 가중치들로 이루어진 (thisLayer size) * (lastLayer size) 크기의 행렬 W
+            self.W = [randomList(lastLayer.size, -1, 1) for _ in range(size)]
+
+            # 레이어 노드들의 활성화함수 입력값들로 이루어진 열벡터 Z
             self.Z = self.feedForward()
-            self.nodeValue = self.activateNode()
 
+            # 레이어 노드들의 활성화함수 출력값들로 이루어진 열벡터 A
+            self.A = self.activateNode()
+
+            # 비용함수의 노드값들에 대한 편미분값으로 이루어진 열벡터 dC_dA
+            self.dC_dA = []
+
+    # Z(n) =  W * A(n-1) + B(n)
     def feedForward(self):
         Z = []
         for nodeIndex in range(self.size):
-            a = 0
+            temp = 0
             for lastNodeIndex in range(self.lastLayer.size):
-                a += self.lastLayer.nodeValue[lastNodeIndex] * self.neuronWeight[nodeIndex][lastNodeIndex]
-            z = a + self.nodeBias[nodeIndex]
+                temp += self.W[nodeIndex][lastNodeIndex] * self.lastLayer.A[lastNodeIndex]
+            z = temp + self.B[nodeIndex]
             Z.append(z)
         return Z
 
+    # A(n) = k(Z(n))
+    # k(x)는 활성화 함수
     def activateNode(self):
         return list(map(lambda x: activationFunc(x), self.Z))
 
-
-def backPropagation(L1, L2):
-    learningRate = 0.5
-    pass
+    def backPropagation(self, L1, L2):
+        learningRate = 0.5
+        pass
 
 
 # 기타 함수들
@@ -58,36 +68,45 @@ def activationFunc(x):
     return y
 
 
-# 입력값의 평균값이 0.5 미만이면 0을, 이상이면 1을 반환한다.
+# 입력값의 평균값이 0.5 미만이면 [0]을, 이상이면 [1]을 반환한다.
 def expectedOutput():
-    if sum(inputLayer.nodeValue) < inputLayer.size / 2:
-        return 0
+    if sum(inputLayer.A) < inputLayer.size / 2:
+        return [0]
     else:
-        return 1
+        return [1]
 
 
+def calculateAverage(lastAverage, n, an):
+    return lastAverage * (n - 1) / n + an / n
+
+
+# C(y) = 1 / n * sum( (Ys - Y)^2 )
+# 비용함수는 오차의 제곱들의 평균값
 def costFunc():
-    y = (expectedOutput() - outputLayer.nodeValue[0]) ** 2
-    return y
+    errorAverage = 0
+    for index in range(outputLayer.size):
+        error = (expectedOutput()[index] - outputLayer.A[index]) ** 2
+        errorAverage = calculateAverage(errorAverage, index + 1, error)
+    return errorAverage
 
 
 def terminalDisplay():
-    print("Input layer node value:", inputLayer.nodeValue)
+    print("Input layer node value:", inputLayer.A)
 
-    print("\nlayer1 node bias:", layer1.nodeBias)
-    print("layer1 neuron weight:", layer1.neuronWeight)
+    print("\nlayer1 node bias:", layer1.B)
+    print("layer1 neuron weight:", layer1.W)
     print("layer1 Z:", layer1.Z)
-    print("layer1 node value:", layer1.nodeValue)
+    print("layer1 node value:", layer1.A)
 
-    print("\nlayer2 node bias:", layer2.nodeBias)
-    print("layer2 neuron weight:", layer2.neuronWeight)
+    print("\nlayer2 node bias:", layer2.B)
+    print("layer2 neuron weight:", layer2.W)
     print("layer2 Z:", layer2.Z)
-    print("layer2 node value:", layer2.nodeValue)
+    print("layer2 node value:", layer2.A)
 
-    print("\nOutput layer node bias:", outputLayer.nodeBias)
-    print("Output layer neuron weight:", outputLayer.neuronWeight)
+    print("\nOutput layer node bias:", outputLayer.B)
+    print("Output layer neuron weight:", outputLayer.W)
     print("Output layer Z:", outputLayer.Z)
-    print("Output layer node value:", outputLayer.nodeValue)
+    print("Output layer node value:", outputLayer.A)
 
     print(expectedOutput())
     print(costFunc())
